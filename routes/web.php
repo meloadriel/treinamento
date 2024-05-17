262f039b617b5c3c27bac0ac8e42d66f7265dc38
Adriel Melo <adrielpakolino@gmail.com>
2024-05-17 11:10:56 -0300

    Criação da view e da rota da view

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Http\Controllers\ContatoController;
 use Illuminate\Support\Facades\Route;
+
+Route::get("/index", [ContatoController::class, 'index'])->name('contatos.index');
 
 Route::get('/', function () {
     return view('welcome');
