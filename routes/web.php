79f6478ef76f30f67042bd858429bf87c19e493e
Adriel Melo <adrielpakolino@gmail.com>
2024-05-21 14:51:39 -0300

    Criação de rotas para todos os métodos restantes

--- routes/web.php
+++ routes/web.php
@@ -4,8 +4,12 @@ use App\Http\Controllers\ContatoController;
 use Illuminate\Support\Facades\Route;
 
 Route::get("/index", [ContatoController::class, 'index'])->name('contatos.index');
-
 Route::get('/create', [ContatoController::class, 'create'])->name('contatos.create');
+Route::post('/store', [ContatoController::class, 'store'])->name('contatos.store');
+Route::get('/show/{id}', [ContatoController::class, 'show'])->name('contatos.show');
+Route::get('/edit/{id}', [ContatoController::class,'edit'])->name('contatos.edit');
+Route::put('/update/{id}', [ContatoController::class,'update'])->name('contatos.update');
+Route::delete('/destroy/{id}', [ContatoController::class, 'destroy'])->name('contatos.destroy');
 
 Route::get('/', function () {
     return view('welcome');
