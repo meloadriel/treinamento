79675246f546ccd49a548bfe2d975eadfd579143
Adriel Melo <adrielpakolino@gmail.com>
2024-05-21 14:52:03 -0300

    Criação da lógica para receber e enviar os dados

--- resources/views/contatos/form.blade.php
+++ resources/views/contatos/form.blade.php
@@ -1,13 +1,57 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
 </head>
+
 <body>
-    {{$categorias}}
-    {{$tipos}}
+    @if (isset($contato))
+        <form action="{{ route('contatos.update', ['id' => $contato->id]) }}" method="POST">
+            @csrf
+            @method('PUT')
+        @else
+            <form action="{{ route('contatos.store') }}" method="POST">
+                @csrf
+    @endif
+
+    <input type="text" id="nome" name="nome"
+        @if (isset($contato)) value='{{ $contato->nome }}' @endif required
+        placeholder="Digite aqui seu nome">
+
+    <input type="text" id="cidade" name="cidade"
+        @if (isset($contato)) value='{{ $contato->endereco->cidade }}' @endif required
+        placeholder= "Digite o nome da sua cidade">
+
+    <input type="text" id="rua" name="rua"
+        @if (isset($contato)) value='{{ $contato->endereco->rua }}' @endif required
+        placeholder= "Digite o nome da sua Rua">
+
+    <input type="text" id="numero" name="numero"
+        @if (isset($contato)) value='{{ $contato->endereco->numero }}' @endif required
+        placeholder= "Digite o numero da sua casa">
+
+    <input type="text" id="telefones" name="telefones[]"
+        @if (isset($contato)) value='{{ $contato->telefone[0]->numero }}' @endif required
+        placeholder="Digite o seu telefone">
+
+    <select name="tipo" id="tipo">
+        @for ($i = 1; $i <= count($tipos); $i++)
+            <option  value="{{ $i }}">{{ $tipos[$i] }}
+            </option>
+        @endfor
+    </select>
+
+    @for ($i = 1; $i <= count($categorias); $i++)
+        <input type="radio" id="{{ $categorias[$i] }}" name="categoria" value="{{ $i }}" checked>
+        <label for="{{ $categorias[$i] }}">{{ $categorias[$i] }}</label>
+    @endfor
+
+    <button type="submit">Criar Contato</button>
+    </form>
 </body>
+
 </html>
--- resources/views/contatos/index.blade.php
+++ resources/views/contatos/index.blade.php
@@ -1,12 +1,53 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
 </head>
+
 <body>
-    {{$contatos}}
+    <a href="{{ route('contatos.create') }}">Criar Contato</a>
+
+    @foreach ($contatos as $contato)
+        <ul>
+            <li>
+                {{ $contato->nome }}
+
+            </li>
+            <li>
+                {{ $contato->endereco->cidade }}
+            </li>
+            <li>
+                {{ $contato->endereco->rua }}
+            </li>
+            <li>
+                {{ $contato->endereco->numero }}
+            </li>
+            @foreach ($contato->telefone as $telefone)
+                <li>
+                    {{ $telefone->numero }}
+                    @if ($telefone->tipo_id == 1)
+                        Fixo
+                    @else
+                        Celular
+                    @endif
+                </li>
+            @endforeach
+            <li>
+                <a href="{{route('contatos.edit', ['id' => $contato->id])}}">Editar Contato</a>
+            </li>
+            <li>
+                <form action="{{route('contatos.destroy', ['id' => $contato->id])}}" method="POST">
+                    @csrf
+                    @method('DELETE')
+                <button type="submit">DELETAR CONTATO</button>
+            </form>
+            </li>
+        </ul>
+    @endforeach
 </body>
+
 </html>
