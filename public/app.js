c422e3207fb576ebec24d6b60ee4635f0987e802
Adriel Melo <adrielpakolino@gmail.com>
2024-05-27 12:40:29 -0300

    Criando novas views (create, edit e show), Removendo a view FORM, e estilizando todas as views!

--- public/app.js
+++ public/app.js
@@ -5,29 +5,28 @@ const divTelefones = document.querySelector("#grupoTelefones");
 
 botaoAdicionarTelefone.addEventListener("click", (ev) => {
     ev.preventDefault();
-    const selects = document.querySelectorAll('select');
-    if(selects.length < 2){
-
+    const selects = document.querySelectorAll("select");
+    if (selects.length < 2) {
         criarCampoTelefone();
-    } else{
-        alert('Você só pode cadastrar dois telefones');
+    } else {
+        alert("Você só pode cadastrar dois telefones");
     }
 });
 
 function criarCampoTelefone() {
     divTelefones.innerHTML += `
-        <div>
-            <input type="text" id="telefones" name="telefones[]" required placeholder="Digite o seu telefone">
 
-             <select name="tipos[]" id="tipo">
-                <option value='1'>
+        <div class="flex px-2">
+            <select name="tipos[]" id="tipo">
+                 <option value='1'>
                     Fixo
-                </option>
-                <option value='2'>
-                    Celular
-                </option>
-            </select>
+                 </option>
+                 <option value='2'>
+                     Celular
+                 </option>
+             </select>
+            <input type="text" id="telefones" name="telefones[]" class="w-full px-2 py-1 text-sm "
+                               required placeholder="Digite o seu telefone">
         </div>
     `;
 }
-
---
+++ resources/views/contatos/create.blade.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Agenda Telefonica</title>
+    @vite('resources/css/app.css')
+</head>
+
+<body class="flex flex-col items-center justify-center min-h-screen py-12 bg-[#ffefcc]">
+    <div
+        class=" overflow-hidden w-[300px] h-[550px] border-[6px] rounded-[30px] border-zinc-900 flex bg-white flex-col justify-start
+    ">
+        <div class="flex items-center justify-between w-full px-6 bg-zinc-200 h-fit">
+            <span class="text-xs">16:20</span>
+            <div class="w-32 h-6 -mt-1 rounded-b-xl bg-zinc-900"></div>
+            <i class="text-xs fa-solid fa-wifi"></i>
+        </div>
+
+
+        <form action="{{ route('contatos.store') }}" method="POST" class="h-full bg-zinc-100">
+            @csrf
+
+            <div class="flex items-center justify-between w-full px-6 py-4 bg-zinc-200 ">
+                <a href="{{ route('contatos.index') }}" class="text-sm text-blue-500">
+                    Cancelar
+                </a>
+                <h1 class="text-lg font-bold">Novo Contato</h1>
+                <button type="submit" class="px-2 text-sm text-white rounded-full hover:text-blue-300">OK</button>
+            </div>
+            <div class="px-2 bg-white">
+                <input type="text" id="nome" name="nome" class="w-full px-2 py-1 text-sm" required
+                    placeholder="Digite aqui seu nome">
+
+                <input type="text" id="cidade" name="cidade" class="w-full px-2 py-1 text-sm border-t " required
+                    placeholder= "Digite o nome da sua cidade">
+
+                <input type="text" id="rua" name="rua" class="w-full px-2 py-1 text-sm border-t " required
+                    placeholder= "Digite o nome da sua Rua">
+
+                <input type="text" id="numero" name="numero" class="w-full px-2 py-1 text-sm border-t " required
+                    placeholder= "Digite o numero da sua casa">
+            </div>
+            <div class="mt-6 bg-white border-t">
+                <div class="flex px-2">
+                    <select name="tipos[]" id="tipo">
+                        @for ($i = 1; $i <= count($tipos); $i++)
+                            <option value="{{ $i }}">{{ $tipos[$i] }}
+                            </option>
+                        @endfor
+                    </select>
+                <input type="text" id="telefones" name="telefones[]" required placeholder="Digite o seu telefone" class="w-full px-2 py-1 text-sm">
+                </div>
+                <div id="grupoTelefones">
+                </div>
+
+            </div>
+            <div class="px-2 py-1 bg-white border-t">
+
+                <button id="botaoAdicionarTelefone" class="flex items-center gap-2">
+                    <span
+                        class="flex items-center justify-center w-5 h-5 text-sm font-bold text-white bg-green-400 rounded-full">+</span>
+                    <span>adicionar telefone</span>
+                </button>
+            </div>
+
+
+            <div class="px-2 py-1 mt-6 bg-white border-t">
+                @for ($i = 1; $i <= count($categorias); $i++)
+                    <input type="radio" id="{{ $categorias[$i] }}" name="categoria" value="{{ $i }}"
+                        checked>
+                    <label for="{{ $categorias[$i] }}">{{ $categorias[$i] }}</label>
+                @endfor
+            </div>
+
+        </form>
+
+        <script src="{{ asset('app.js') }}"></script>
+        <script src="https://kit.fontawesome.com/573e44f26e.js" crossorigin="anonymous"></script>
+</body>
+
+</html>
