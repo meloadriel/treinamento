f67b30fc44a804e2b8e2ac817b7b6ed54b8dea85
Adriel Melo <adrielpakolino@gmail.com>
2024-05-27 12:39:45 -0300

    Instalação e configuração do Tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+    content: [
+        "./resources/**/*.blade.php",
+        "./resources/**/*.js",
+        "./resources/**/*.vue",
+    ],
 
+    theme: {
+        extend: {
+            width: {
+                300: "300px",
+            },
+            height: {
+                500: "500px",
+            },
+        },
+    },
+    plugins: [],
+};
